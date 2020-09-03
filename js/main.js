f863add3e747dff652d5f7a5f8d0f0c3dab604c7
nickelyn <kknicklas3689@hotmail.com>
2020-09-03 21:27:33 +0200

    add steam stats

--- js/main.js
+++ js/main.js
@@ -1,26 +1,49 @@
 console.log('This would be the main JS file.');
-/*
-function getSteamProfile() {
-  var id = '76561198042014259'
-  var key = '8DDDBE9C6A344ECDA9EC11B6BA6C38DD'
-  var url = `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${key}&steamids=${id}`
-*/
 
-const api = {
-
-  url: 'https://api.steampowered.com/ISteamUser/',
-  key: '8DDDBE9C6A344ECDA9EC11B6BA6C38DD',
-  steamid: '76561198042014259',
+const apiData = {
+  url: 'https://cors-anywhere.herokuapp.com/https://api.steampowered.com/ISteamUser/',
+  type: '8DDDBE9C6A344ECDA9EC11B6BA6C38DD',
+  id: '76561198042014259',
 }
 
-const {url, key, steamid} = api
-const ProfileURL = `${url}GetPlayerSummaries/v0002/?key=${key}&steamids=${steamid}`
-console.log(ProfileURL)
+const {
+  url,
+  type,
+  id
+} = apiData
 
-fetch('https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=94E8C5B9F09CF7F6B5D46CBBA8D7DF30&steamids=76561198042014259')
-   .then( response => response.json() )
-   .then( data => console.log(data) )
-/*fetch(ProfileURL)
-  .then( (data) => console.log(data.json()))
-  .then( (personaState) => console.log(pokemon.json()))
-*/
+const apiUrl = `${url}GetPlayerSummaries/v0002/?key=${type}&steamids=${id}`
+
+fetch(apiUrl)
+  .then((data) => {
+    if (data.ok) {
+      return data.json()
+    }
+    throw new Error('Response not ok.');
+  })
+  .then(steaminfo => generateHtml(steaminfo))
+  .catch(error => console.error('Error:', error))
+
+
+const generateHtml = (data) => {
+  console.log(data)
+  var player = data.response.players[0]
+  var state = player.personastate
+  var img = player.avatarfull
+  var game = player.gameextrainfo
+  const html = `
+        <table class="details">
+        <tr>
+        <td>STATUS: </td>
+        <td id="personastate${state == 1 && game == undefined ? 1 : state == 0 ? 0 : 6}">
+        ${state == "0" ? "OFFLINE" : state == "1" && game == undefined  ? "ONLINE" : game}</td></tr>
+
+        <img src=${img} class="avatar" id="personastate${state == 1 && game == undefined ? 1 : state == 0 ? 0 : 6}">
+        </table>
+    `
+  console.log(player.timecreated)
+  console.log(game)
+  console.log(state);
+  const newDiv = document.querySelector('.usercontent')
+  newDiv.innerHTML = html
+}
