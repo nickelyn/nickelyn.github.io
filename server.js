eae39b0e808fc6c6ed3d18e2bf09e2baa8ebdfcb
nickelyn <kknicklas3689@hotmail.com>
2021-02-08 17:04:58 +0100

    im retard

---
+++ server.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+const port = 3000;
+
+http.createServer(function (request, response) {
+    //console.log('request ', request.url);
+
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+        filePath = './index.html';
+    }
+
+    var extname = String(path.extname(filePath)).toLowerCase();
+    var mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.gif': 'image/gif',
+        '.svg': 'image/svg+xml',
+        '.wav': 'audio/wav',
+        '.mp4': 'video/mp4',
+        '.woff': 'application/font-woff',
+        '.ttf': 'application/font-ttf',
+        '.eot': 'application/vnd.ms-fontobject',
+        '.otf': 'application/font-otf',
+        '.wasm': 'application/wasm'
+    };
+
+    var contentType = mimeTypes[extname] || 'application/octet-stream';
+}).listen(port);
+console.log('Server running at localhost:'+port);
